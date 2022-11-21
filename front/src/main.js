aab0597566bcd2c22d1bd0e92e4d4c71ceb303c6
junseok9505 <tradition@kakao.com>
2022-11-21 10:47:27 +0900

    2022-11-21 10:47 sjs

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '@/routes/routes.js'
+import router from '@/routes/routes.ts'
 
 const app = createApp(App)
 app.use(router)
